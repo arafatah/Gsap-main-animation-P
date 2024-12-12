cc050cac6b9d9110de6f2de97b98ac7bb4d55ff3
arafatah <arafathowlader890@gmail.com>
2024-12-12 17:22:04 +0600

    implement gsap

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('click', function (event) {
+    if (event.target.matches('button')) {
+      event.target.focus()
+    }
+  })
+  
+  
+  var tween = gsap.to(".green", {duration:3, x:600, ease:"linear", paused:true});
+  
+  
+  document.getElementById("play").onclick = ()=> tween.play();
+  document.getElementById("pause").onclick = ()=> tween.pause();
+  document.getElementById("reverse").onclick = ()=> tween.reverse();
+  document.getElementById("restart").onclick = ()=> tween.restart();
